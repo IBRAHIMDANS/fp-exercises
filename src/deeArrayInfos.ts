3c7293f5123911e32ef11adda789284032febca4
Ibrahima DANSOKO <ibrahima.Dansoko@outlook.com>
2020-08-25 09:12:08 +0200

    init

---
+++ src/deeArrayInfos.ts
@@ -0,0 +1,14 @@
+// Write a functions that :
+//
+// calculate the sum of a deep array
+// calculate the depth of the array
+// find the largest value in a tree
+// find the smallest value in a tree
+// Example of an array : [1, [[2], 3], [4], 5, [6, 42, [[86], [[12]], 1337]], 1]
+
+const sum = (a: number, b: number): number => a+b;
+const arrayExample =  [1, [[2], 3], [4], 5, [6, 42, [[86], [[12]], 1337]], 1]
+
+const sumDeepArray= (deep: Array<number>) => {
+
+}
